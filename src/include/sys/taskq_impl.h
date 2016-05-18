0819bc469da97901dff1a83e48b805e859f1c40d
James Peach <jpeach@apache.org>
2016-05-17 19:35:10 -0700

    Add various stub and forwarding headers.

---
+++ src/include/sys/taskq_impl.h
@@ -0,0 +1,29 @@
+/** @file
+ *
+ *  A brief file description
+ *
+ *  @section license License
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef TASKQ_IMPL_H_181612B6_988D_4310_8255_B7750D75D4A8
+#define TASKQ_IMPL_H_181612B6_988D_4310_8255_B7750D75D4A8
+
+#include <spl/taskq_impl.h>
+
+#endif /* TASKQ_IMPL_H_181612B6_988D_4310_8255_B7750D75D4A8 */
