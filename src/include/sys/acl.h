00e74959c8b6b89374936a8a8e60cf314f8bc90f
James Peach <jpeach@apache.org>
2016-05-19 18:24:21 -0700

    Add sys/acl.h compatibility header.

---
+++ src/include/sys/acl.h
@@ -0,0 +1,33 @@
+
+/** @file
+ *
+ *  A brief file description
+ *
+ *  @section license License
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+#ifndef ACL_H_BA8DB050_85AF_406C_AA2C_B446C1E15F39
+#define ACL_H_BA8DB050_85AF_406C_AA2C_B446C1E15F39
+
+#include_next <sys/acl.h>
+
+typedef acl_entry_t ace_t;
+
+#endif /* ACL_H_BA8DB050_85AF_406C_AA2C_B446C1E15F39 */
