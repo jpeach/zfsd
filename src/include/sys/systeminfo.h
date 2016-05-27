2ef23c19cfa86c25908f18051aeec158c6b217ab
James Peach <jpeach@apache.org>
2016-05-26 21:42:44 -0700

    Add various stub headers.

---
+++ src/include/sys/systeminfo.h
@@ -0,0 +1,29 @@
+
+/** @file
+ *
+ *  A brief file description
+ *
+ *  @section license License
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+#ifndef SYSTEMINFO_H_246D9931_A64B_40F1_AFD5_9715A14B06C5
+#define SYSTEMINFO_H_246D9931_A64B_40F1_AFD5_9715A14B06C5
+
+#endif /* SYSTEMINFO_H_246D9931_A64B_40F1_AFD5_9715A14B06C5 */
